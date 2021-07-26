237a354f85a4411ee9b6678c9a40b4ef255626c9
Chandra Chinta <sundar4chandu@gmail.com>
2021-07-26 01:02:28 -0400

    Updated home page to search for a user

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController, ToastController } from '@ionic/angular';
+import { GitApiService } from '../services/git-api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,17 +9,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  public commitList;
+  public username: string = '';
+  public users;
 
   constructor(private gitService: GitApiService,
               private navCtrl: NavController,
               private toastCtrl: ToastController) {}
 
+  searchUser(){
+    if(this.username){
+      this.gitService.searchUser(this.username).then((res:any) => {
+        console.log(res);
+        this.users = res.items
+      }).catch(err => {
+        this.toastCtrl.create({
+          message: 'Something went wrong',
+          duration: 3000,
+          position: 'top'
+        }).then(toast => toast.present());
+      })
+    }
+  }
 
   getRecentCommits(name?){
     let params = {};
     if(name) params = {username : name}
     this.navCtrl.navigateForward(['/commits'], {queryParams: params});
+    this.users = undefined;
+    this.username = '';
   }
 
 }
